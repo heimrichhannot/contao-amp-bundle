6ad5310c2b000ef799e16b402b0044b85186c970
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-10-19 16:11:42 +0200

    added more functionality

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\AmpBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+
+        $rootNode = $treeBuilder->root('huh');
+
+        $rootNode
+            ->children()
+                ->arrayNode('amp')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->arrayNode('elements')
+                            ->arrayPrototype()
+                                ->children()
+                                    ->scalarNode('name')
+                                        ->isRequired()
+                                        ->cannotBeEmpty()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
